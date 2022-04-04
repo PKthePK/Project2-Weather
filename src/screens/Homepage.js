9fe0fab9b90c769a9996212df3586f0576ef5bce
PKthePK <pkwoo90@gmail.com>
2022-04-04 12:18:29 +0800

    Update

--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -7,7 +7,7 @@ import {
     withRouter,
     NavLink,
 } from 'react-router-dom';
-import '../styles/styles.css'
+import '../assets/styles/styles.css'
 import {TwoHoursNowcast, TwentyfourHoursForecast, FourDaysForecast, PSI, UV} from '../routes/routes';
 
 class Home extends React.Component {
@@ -21,33 +21,33 @@ class Home extends React.Component {
                 <Router basename='/Project2-Weather'>
                     <div className='navBar'>
                         <div>
-                            <Link to='/'></Link>
                             <NavLink className='link' to ='/2hrs'>2 Hours Nowcast</NavLink>
                             <NavLink className='link'to ='/24hrs'>24 Hours Forecast</NavLink>
                             <NavLink className='link'to ='/4days'>4 Days Forecast</NavLink>
                             <NavLink className='link'to ='/PSI'>PSI</NavLink>
                             <NavLink className='link'to ='/UV'>UV</NavLink>
+                            <Link to='/'></Link>
                         </div>
                     </div>
                     <div className="mapArea">
                         <Switch>
-                            <Route exact path='/'>
-                                <h2> This is the empty page</h2>
-                            </Route>
                             <Route className='leaflet-container' path='/2hrs'>
                                 <TwoHoursNowcast />
                             </Route>
-                            <Route path='/24hrs'>
+                            <Route className='leaflet-container' path='/24hrs'>
                                 <TwentyfourHoursForecast />
                             </Route>
                             <Route path='/4days'>
                                 <FourDaysForecast />
                             </Route>
-                            <Route path='/PSI'>
+                            <Route className='leaflet-container' path='/PSI'>
                                 <PSI />
                             </Route>
                             <Route path='/UV'>
                                 <UV />
+                            </Route>
+                            <Route exact path='/'>
+                                <h2> This is the empty page</h2>
                             </Route>
                         </Switch>
                     </div>
